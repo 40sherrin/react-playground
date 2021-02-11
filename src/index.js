d627523eb3057c5b899292f97d3c4d9eb0eff1a0
40sherrin <fareedaskari42@gmail.com>
2021-02-11 21:17:57 +1100

    Component workout

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from "react-dom";
 function Greeting(){
   return (
       <div>
+        <Person />
         <h1>Hard work will pay</h1>
         <p>displine would be the key</p>
       </div>
   )
 };
 
+const Person = () => <h1>This is Fareed Askari</h1>;
 
 ReactDOM.render(<Greeting />, document.getElementById("root"));
