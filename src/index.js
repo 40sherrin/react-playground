8ff87723816250e0cd7dc0171156eea245f53e61
40sherrin <fareedaskari42@gmail.com>
2021-02-20 12:50:11 +1100

    test commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import data from "./data";
-import Book from "./book";
 import "./index.css";
 
 
 
 const BookLists = () => {
-  return (
-    <section>
-      {
-        data.map((book, index) => {
-          return <Book key={book.id} {...book} />
-        })
-      }
-    </section>
-  )
+  return <>
+    <div className="container">
+        <h1>Hello world</h1>
+    </div>
+  </>;
 };
 
 
